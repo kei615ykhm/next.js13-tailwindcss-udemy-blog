aeccb62dd495ee9db45403134f94d6db546b9384
kei <from0615.ykhm@gmail.com>
2024-08-06 19:53:32 +0900

    add: Headerの文字にホームページリンクを設定
    - `Next.js13 Blog`をクリックするとホームページに戻るようにリンクを設定した

--- src/app/PageHeader.tsx
+++ src/app/PageHeader.tsx
@@ -1,10 +1,11 @@
+import Link from "next/link";
 import React from "react";
 
 export const PageHeader: React.FC = () => {
   return (
     <header>
       <div>
-        <h1>Next.js13 Blog</h1>
+        <Link href="/">Next.js13 Blog</Link>
       </div>
     </header>
   );
