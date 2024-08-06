a2f915d3f6ed7e7747c29615a5fea15428c2dc9a
kei <from0615.ykhm@gmail.com>
2024-08-06 20:56:51 +0900

    remove: レイアウト調整のため、一時的にホームボタンを削除

--- src/app/PageHeader.tsx
+++ src/app/PageHeader.tsx
@@ -4,11 +4,7 @@ import React from "react";
 export const PageHeader: React.FC = () => {
   return (
     <header className="py-5 px-10 border-b flex justify-between item-cen">
-      <div>
-        <h1 className="text-2x1 font-extrabold">
-          <Link href="/">Next.js13 Blog</Link>
-        </h1>
-      </div>
+      <div></div>
       <div>
         <nav>
           <Link href="/articles/new">記事を書く</Link>
