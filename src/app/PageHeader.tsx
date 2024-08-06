ed841d97ef4f73c4b527f1b648cde3716f28dcbe
kei <from0615.ykhm@gmail.com>
2024-08-06 20:01:35 +0900

    update: Headerのリンクを太字に変更

--- src/app/PageHeader.tsx
+++ src/app/PageHeader.tsx
@@ -5,7 +5,9 @@ export const PageHeader: React.FC = () => {
   return (
     <header>
       <div>
-        <Link href="/">Next.js13 Blog</Link>
+        <h1 className="text-2x1 font-extrabold">
+          <Link href="/">Next.js13 Blog</Link>
+        </h1>
       </div>
     </header>
   );
