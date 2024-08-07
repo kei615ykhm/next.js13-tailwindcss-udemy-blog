d86fab549abb77bbed71fc9f9b6ce909d43f157a
kei <from0615.ykhm@gmail.com>
2024-08-07 23:04:35 +0900

    rename: デフォルトエクスポートに変更
    - Issue #7 に対するフィードバックの検証。Headerのimportをしていないだけという可能性があるため、ソースコード通りに修正。

--- src/app/Footer.tsx
+++ src/app/Footer.tsx
@@ -1,11 +1,11 @@
 import React from "react";
 
-const PageFooter = () => {
+export default function Footer() {
   return (
     <footer className="py-2 px-4 border-t">
       <small>@2023 shincode</small>
     </footer>
   );
-};
+}
 
-export default PageFooter;
+export default Footer;
