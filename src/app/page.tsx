aa55671e98cfb78c4a2ecec4630131d3f7ba745a
kei <from0615.ykhm@gmail.com>
2024-08-08 23:41:24 +0900

    add: `flex`を使って`aside`要素の横並びのレイアウトを調整

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,7 @@ import ArticleList from "./components/ArticleList";
 export default function Home() {
   return (
     <div className="md:flex">
-      <section>
+      <section className="w-full md:w-2/3 flex flex-col items-center px-3">
         <ArticleList />
       </section>
 
