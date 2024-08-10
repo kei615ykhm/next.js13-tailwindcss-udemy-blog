eb7d3ae2434a4d5435a7cbc8177ba27b28030d21
kei <from0615.ykhm@gmail.com>
2024-08-10 21:33:00 +0900

    add: `unsplash`の設定を追加
    - Imageのsrcに読み込みエラーが出ているため、`next.config.js`の調整を同時に行っている

--- src/app/components/ArticleList.tsx
+++ src/app/components/ArticleList.tsx
@@ -1,7 +1,22 @@
+import Image from "next/image";
+import Link from "next/link";
 import React from "react";
 
 const ArticleList = () => {
-  return <div>ArticleList</div>;
+  return (
+    <div>
+      <article>
+        <Link href="#">
+          <Image
+            src="https://source.unsplash.com/collection/1346951/1000x500?sig=1"
+            alt=""
+            widts={1280}
+            height={300}
+          />
+        </Link>
+      </article>
+    </div>
+  );
 };
 
-export default ArticleListce;
+export default ArticleList;
