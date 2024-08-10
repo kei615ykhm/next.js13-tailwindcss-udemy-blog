651c9694ef0c076a57ca39226b5ac7fa990dd20e
kei <from0615.ykhm@gmail.com>
2024-08-10 21:35:05 +0900

    remove: `ArticleLis.tsx`のunsplash設定のために既存コードを削除

--- next.config.js
+++ next.config.js
@@ -1,11 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    domains: ["source.unsplash.com"],
-  },
-  experimental: {
-    appDir: true,
-  },
 };
 
 module.exports = nextConfig;
