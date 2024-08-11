d6fb057d7a8493d5c39f64c57506ad3d13ac256e
kei <from0615.ykhm@gmail.com>
2024-08-11 15:24:03 +0900

    add: ブログ投稿時のURLに関連する機能の作成を開始

--- src/app/articles/new/page.tsx
+++ src/app/articles/new/page.tsx
@@ -1,7 +1,21 @@
 import React from "react";
 
 const CreateBlogPage = () => {
-  return <div>新規作成</div>;
+  return (
+    <div>
+      <h2>ブログ新規作成</h2>
+
+      <form>
+        <div>
+          <label>URL</label>
+          <input
+            type="text"
+            className="shadow border round w-full py-2 px-3 text-gray-700 leading-tight"
+          />
+        </div>
+      </form>
+    </div>
+  );
 };
 
 export default CreateBlogPage;
