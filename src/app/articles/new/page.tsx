2f7043e2c72c2bf8e343ee04a55d8bb25782a262
kei <from0615.ykhm@gmail.com>
2024-08-11 15:36:25 +0900

    update: フォームのレイアウトを変更
    - URLとタイトルを別々で個別で記述できるようにする項目を追加した

--- src/app/articles/new/page.tsx
+++ src/app/articles/new/page.tsx
@@ -5,16 +5,25 @@ const CreateBlogPage = () => {
     <div>
       <h2>ブログ新規作成</h2>
 
-      <form>
+      <form className="bg-slate-200 p-6 rounded shadow-lg">
         <div className="mb-4">
-          <label>URL</label>
+          <label className="text-gray-700 text-sm font-bold mb-2">URL</label>
           <input
             type="text"
             className="shadow border round w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none"
           />
         </div>
         <div className="mb-4">
-          <label>URL</label>
+          <label className="text-gray-700 text-sm font-bold mb-2">
+            タイトル
+          </label>
+          <input
+            type="text"
+            className="shadow border round w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none"
+          />
+        </div>
+        <div className="mb-4">
+          <label className="text-gray-700 text-sm font-bold mb-2">本文</label>
           <input
             type="text"
             className="shadow border round w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none"
