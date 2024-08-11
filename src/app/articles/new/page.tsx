1b9d10ec277706dfe81a63c0fc5139c2bc88edb5
kei <from0615.ykhm@gmail.com>
2024-08-11 15:27:56 +0900

    add: labelの基本的なレイアウトを作成

--- src/app/articles/new/page.tsx
+++ src/app/articles/new/page.tsx
@@ -6,11 +6,18 @@ const CreateBlogPage = () => {
       <h2>ブログ新規作成</h2>
 
       <form>
-        <div>
+        <div className="mb-4">
           <label>URL</label>
           <input
             type="text"
-            className="shadow border round w-full py-2 px-3 text-gray-700 leading-tight"
+            className="shadow border round w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none"
+          />
+        </div>
+        <div className="mb-4">
+          <label>URL</label>
+          <input
+            type="text"
+            className="shadow border round w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none"
           />
         </div>
       </form>
