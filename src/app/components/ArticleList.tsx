f77e887a3d7ea33b5a7964b34a78a8e6f235dabf
kei <from0615.ykhm@gmail.com>
2024-08-11 14:48:04 +0900

    add: `By kei615ykhm, 2024/08/10`部分を調整
    - 他の要素と同じように白背景でも見やすくなる調整を行った

--- src/app/components/ArticleList.tsx
+++ src/app/components/ArticleList.tsx
@@ -24,7 +24,9 @@ const ArticleList = () => {
           >
             Next.jsの勉強中
           </Link>
-          <p>By kei615ykhm, 2024/08/10</p>
+          <p className="text-sm pb-3 text-slate-900">
+            By kei615ykhm, 2024/08/10
+          </p>
           <Link href="#" className="text-slate-900 pb-6">
             Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolor,
             placeat. Veritatis, debitis quidem. Fugiat alias assumenda sequi
