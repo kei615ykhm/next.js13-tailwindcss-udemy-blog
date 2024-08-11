7b472dea94772caccce054de960cbae614830c69
kei <from0615.ykhm@gmail.com>
2024-08-11 15:02:44 +0900

    add: 下記事として使用する`article`要素をひとつ追加
    - 記事投稿によりデータが増えた時の様子を示唆（教材による指示）

--- src/app/components/ArticleList.tsx
+++ src/app/components/ArticleList.tsx
@@ -36,6 +36,37 @@ const ArticleList = () => {
           </Link>
         </div>
       </article>
+      <article className="shadow my-4 flex flex-col">
+        <Link href="#" className="hover:opacity-75">
+          <Image
+            src="https://picsum.photos/seed/picsum/1000/500"
+            alt=""
+            width={1280}
+            height={300}
+          />
+        </Link>
+        <div className="bg-white flex flex-col  justify-start p-6">
+          <Link href="#" className="text-blue-700 pb-4 font-bold">
+            technology
+          </Link>
+          <Link
+            href="#"
+            className="text-slate-900 text-3xl font-bold hover:text-gray-700"
+          >
+            Reactの勉強中
+          </Link>
+          <p className="text-sm pb-3 text-slate-900">published on 2024/08/11</p>
+          <Link href="#" className="text-slate-900 pb-6">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Quia
+            voluptas, nobis fugit aut obcaecati, corrupti voluptate harum nulla
+            hic nihil vitae sapiente exercitationem voluptatibus doloribus quasi
+            impedit quisquam odit debitis!
+          </Link>
+          <Link href="#" className="text-pink-800 hover:text-black">
+            続きを読む
+          </Link>
+        </div>
+      </article>
     </div>
   );
 };
