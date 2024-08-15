e9949591f986d89b11bbe04a5077d25b00f0398f
kei <from0615.ykhm@gmail.com>
2024-08-15 23:03:01 +0900

    add: `ArticleList`コンポーネントを追加して記事リストを表示
    - `articles`プロパティを受け取って記事のタイトルリストをレンダリングできるようにした

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,7 +8,7 @@ export default async function Home() {
   return (
     <div className="md:flex">
       <section className="w-full md:w-2/3 flex flex-col items-center px-3">
-        <ArticleList />
+        <ArticleList articles={articles} />
       </section>
 
       <aside className="w-full md:w-1/3 flex flex-col items-center px-3 md:pl-6">
