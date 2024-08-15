dcae344b9d57075faa0bbfe00052dcf9b82b9f0b
kei <from0615.ykhm@gmail.com>
2024-08-15 20:54:00 +0900

    update: 関数の誤植を修正
    - `getAllposts`から`getAllArticles`に変更

--- src/blogAPI.ts
+++ src/blogAPI.ts
@@ -1,9 +1,9 @@
 import { Article } from "./types";
 
 // type.tsのArticle型を返す関数
-export const getAllPosts = async (): Promise<Article[]> => {
+export const getAllArticles = async (): Promise<Article[]> => {
   // fetchでAPIを叩いてhttpsにGETリクエストを送る
-  const res = await fetch(`https://localhost:3001/posts`, {
+  const res = await fetch(`http://localhost:3001/posts`, {
     cache: "no-cache",
   }); // ここでSSRのAPIを叩いている
 
